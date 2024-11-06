414500ac6c9eb5db462e19429cc31a20f5fcfec6
Takahiro Ogawa <boroney1@gmail.com>
2024-11-06 16:29:19 +0900

    Commit

--- app.vue
+++ app.vue
@@ -1,6 +1,31 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-white dark:bg-dim-900">
+    <div class="min-h-full">
+
+      <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+
+        <!-- Left sidebar -->
+        <div class="hidden md:block xs:col-span-1 xl:col-span-2">
+          <div class="sticky top-0">
+            <SidebarLeft />
+          </div>
+        </div>
+
+        <!-- Main content -->
+        <!-- <main class="col-span-12 md:col-span-8 xl:col-span-6 bg-slate-200">
+          <h1>aaa</h1>
+        </main> -->
+
+        <!-- Right sidebar -->
+        <div class="hidden md:block  xl:col-span-4 md:col-span-3 bg-slate-200">
+          <div class="sticky top-0">
+            <h1>bbb</h1>
+          </div>
+        </div>
+
+
+      </div>
+
+    </div>
   </div>
 </template>
